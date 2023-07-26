2e92d1a3b1a2c714425d11286475d3d53fd365e7
Akram Mahmoud <akrammahmoud.web@gmail.com>
2023-07-26 17:28:12 +0200

    feat: handle api error

--- src/utils/getSuggestionsApi.ts
+++ src/utils/getSuggestionsApi.ts
@@ -8,16 +8,20 @@ const getSuggestionsApi = async (
   query: string,
   signal?: AbortSignal
 ): Promise<string[]> => {
-  // fetch https://api.spoonacular.com/recipes/complexSearch?apiKey=01894ee9e1e64c7e8a2ccf68393d17f6&query=canne
-  const response = await fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=01894ee9e1e64c7e8a2ccf68393d17f6&query=${query}`,
-    { signal }
-  );
-  const data = await response.json();
-  const filteredSuggestions = data.results.map(
-    (suggestion: { title: string }) => suggestion.title
-  );
-  return filteredSuggestions;
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=01894ee9e1e64c7e8a2ccf68393d17f6&query=${query}`,
+      { signal }
+    );
+    const data = await response.json();
+    const filteredSuggestions = data.results.map(
+      (suggestion: { title: string }) => suggestion.title
+    );
+    return filteredSuggestions;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
 
 export default getSuggestionsApi;
